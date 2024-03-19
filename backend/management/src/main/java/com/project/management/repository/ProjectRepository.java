15a426263d72dc7f0ae91b57d4ea2658f35df1af
jeyhan22 <74844330+jeyhan22@users.noreply.github.com>
2024-03-19 16:47:26 +0800

    Project management operations

---
+++ backend/management/src/main/java/com/project/management/repository/ProjectRepository.java
@@ -0,0 +1,7 @@
+package com.project.management.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.project.management.entity.Project;
+
+public interface ProjectRepository extends JpaRepository<Project, Long>{
+}
