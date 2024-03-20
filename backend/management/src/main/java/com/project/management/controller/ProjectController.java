b5f0a57a81508af8e5b1a40502ca26c06120305a
jeyhan22 <74844330+jeyhan22@users.noreply.github.com>
2024-03-20 20:37:04 +0800

    Enable Cross Origin

--- backend/management/src/main/java/com/project/management/controller/ProjectController.java
+++ backend/management/src/main/java/com/project/management/controller/ProjectController.java
@@ -9,6 +9,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -23,6 +24,7 @@ import com.project.management.service.ProjectService;
 
 @RestController
 @RequestMapping("/api/projects")
+@CrossOrigin("http://localhost:3000")
 public class ProjectController {
     
     @Autowired
