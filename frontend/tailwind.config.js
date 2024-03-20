13b82ed0b9c14b41ca032428195162b0acbd7e6d
jeyhan22 <74844330+jeyhan22@users.noreply.github.com>
2024-03-20 20:37:59 +0800

    Add fonts

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -2,7 +2,13 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        montserrat: ['Montserrat', 'sans-serif'],
+        courgette: ['Courgette', 'cursive'],
+        unbounded: ['Unbounded'],
+      },
+    },
   },
-  plugins: [],
+  plugins: [require('daisyui')],
 };
