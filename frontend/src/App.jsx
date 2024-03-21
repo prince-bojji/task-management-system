25412b1325f5cedb0a5fc658f1454e4c3a643894
jeyhan22 <74844330+jeyhan22@users.noreply.github.com>
2024-03-21 21:25:51 +0800

    Configure routing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,34 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Login from './pages/users/Login';
+import Landing from './pages/Landing';
+import Signup from './pages/users/Signup';
+import UserLayout from './components/UserLayout';
+import Home from './pages/users/Home';
+import Project from './pages/users/Project';
+import Profile from './pages/users/Profile';
+import { UserProvider } from './components/UserContext';
+
 export default function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-  )
+    <UserProvider>
+      <Router>
+        <div className='flex flex-col min-h-screen'>
+          <div className='flex-1'>
+            <Routes>
+              <Route element={<UserLayout />}>
+                <Route exact path='/home' element={<Home />} />
+                <Route exact path='/project' element={<Project />} />
+                <Route exact path='/profile' element={<Profile />} />
+              </Route>
+              {/* Do the same for admin */}
+              <Route exact path='/userSignup' element={<Signup />} />
+              <Route exact path='/userLogin' element={<Login />} />
+              <Route exact path='/' element={<Landing />} />
+            </Routes>
+          </div>
+        </div>
+      </Router>
+    </UserProvider>
+  );
 }
