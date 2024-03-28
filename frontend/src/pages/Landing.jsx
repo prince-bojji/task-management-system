322aa877c1c1e512e9e6372f010132b7d3e8294d
jeyhan22 <74844330+jeyhan22@users.noreply.github.com>
2024-03-28 13:36:57 +0800

    Landing page

--- frontend/src/pages/Landing.jsx
+++ frontend/src/pages/Landing.jsx
@@ -1,12 +1,18 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+/**
+ * Landing component representing the landing page of the application.
+ * Displays options for user and admin login/signup.
+ *
+ * @returns JSX element representing the landing page.
+ */
 function Landing() {
   return (
     <div className='flex justify-center items-center min-h-screen bg-gray-100 font-montserrat'>
       <div className='w-full max-w-md bg-white p-8 rounded-md shadow-md'>
         <h2 className='text-2xl font-bold mb-6 text-center text-[#5d7468]'>
-          WELCOME
+          Welcome
         </h2>
         <div className='mb-4 flex justify-center'>
           <Link
