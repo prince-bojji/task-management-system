9c0efa7a784e502056281b9a8e037acfe791e960
jeyhan22 <74844330+jeyhan22@users.noreply.github.com>
2024-06-24 14:32:45 +0800

    add some modification, will fix the other later

--- backend/management/src/main/java/com/project/management/entity/Project.java
+++ backend/management/src/main/java/com/project/management/entity/Project.java
@@ -1,45 +1,45 @@
-// /**
-//  * Represents a project entity.
-//  */
+/**
+ * Represents a project entity.
+ */
 
-// package com.project.management.entity;
+package com.project.management.entity;
 
-// import jakarta.persistence.Column;
-// import jakarta.persistence.Entity;
-// import jakarta.persistence.GeneratedValue;
-// import jakarta.persistence.GenerationType;
-// import jakarta.persistence.Id;
-// import jakarta.persistence.JoinColumn;
-// import jakarta.persistence.ManyToOne;
-// import jakarta.persistence.Table;
-// import lombok.Getter;
-// import lombok.Setter;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
 
-// @Getter
-// @Setter
-// @Entity
-// @Table(name = "projects")
-// public class Project {
+@Getter
+@Setter
+@Entity
+@Table(name = "projects")
+public class Project {
     
-//     @Id
-//     @GeneratedValue(strategy = GenerationType.IDENTITY)
-//     @Column(name = "project_id")
-//     private Long projectId;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "project_id")
+    private Long projectId;
 
-//     @Column(name = "project_name")
-//     private String projectName;
+    @Column(name = "project_name")
+    private String projectName;
 
-//     @Column(name = "project_details")
-//     private String projectDetails;
+    @Column(name = "project_details")
+    private String projectDetails;
 
-//     @Column(name = "project_deadline")
-//     private String projectDeadline;
+    @Column(name = "project_deadline")
+    private String projectDeadline;
 
-//     @Column(name = "project_status")
-//     private String projectStatus;
+    @Column(name = "project_status")
+    private String projectStatus;
 
-//     @ManyToOne
-//     @JoinColumn(name = "user_id")
+    @ManyToOne
+    @JoinColumn(name = "user_id")
 
-//     private  User assignedUser;
-// }
+    private  User assignedUser;
+}
--- backend/management/src/main/java/com/project/management/repository/ProjectRepository.java
+++ backend/management/src/main/java/com/project/management/repository/ProjectRepository.java
@@ -1,7 +1,7 @@
-// package com.project.management.repository;
+package com.project.management.repository;
 
-// import org.springframework.data.jpa.repository.JpaRepository;
-// import com.project.management.entity.Project;
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.project.management.entity.Project;
 
-// public interface ProjectRepository extends JpaRepository<Project, Long>{
-// }
+public interface ProjectRepository extends JpaRepository<Project, Long>{
+}
